49eae56d304001eb6ccc3599ed2eb96cb5720664
moshe <moshe.beeri@gmail.com>
2020-11-07 20:32:03 +0200

    test framework setup

--- tests/simple_ping_test.py
+++ tests/simple_ping_test.py
@@ -1,10 +1,22 @@
+from mock import patch, Mock
 import pytest
+from pytest_mock import mocker
+from commands.client import Client
+from commands.server import Server
 
-class TestClass:
-  def inc(self, x):
-    return x + 1
+class TestSimpleClientSeverPing:
+  def create_client(self, protocol, ip, port):
+    return Client
 
-  def test_answer(self):
-    print('test')
-    assert self.inc(3) == 4
+  def create_server(self, protocol, ip, port):
+    return Server
+  
+  @patch('commands.client.Client.ping', return_value=1)
+  def test_ping(self, mocker):
+    # mocker.patch('commands.client.Client.ping', return_value=1)
+
+    client = self.create_client( "UDP", "127.0.0.1", 0)
+    server = self.create_server( "UDP", "127.0.0.1", 0)
+    time = client.ping()
+    assert time <= 1
 
